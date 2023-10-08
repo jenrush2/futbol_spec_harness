7902675de9f6ad6dd9ebd33f702e28450a0d12ee
Jennifer Rush <jennifer.rush2@gmail.com>
2023-10-07 19:04:03 -0600

    create favorite opponent

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -129,11 +129,11 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.most_goals_scored("18")).to eq 7
   end
 #not in iteration 2
-  it "#fewest_goals_scored" do
+  xit "#fewest_goals_scored" do
     expect(@stat_tracker.fewest_goals_scored("18")).to eq 0
   end
 #not in iteration 2
-  xit "#favorite_opponent" do
+  it "#favorite_opponent" do
     expect(@stat_tracker.favorite_opponent("18")).to eq "DC United"
   end
 #not in iteration 2
